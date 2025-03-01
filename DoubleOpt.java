06a54ae2ae68621e944928cefa897309c8cb3126
Akki <chauhanakshit61@gmail.com>
2025-03-01 09:53:41 +0530

    [Akshit] ADD: Performing double operations

---
+++ DoubleOpt.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DoubleOpt {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Enter the value of b: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Enter the value of c: ");
+        double c = scanner.nextDouble();
+
+        scanner.close();
+
+        double result1 = a + b * c;
+        double result2 = a * b + c;
+        double result3 = c + a / b;
+        double result4 = a % b + c;
+
+        System.out.printf("The results of Double Operations are %.2f, %.2f, %.2f, and %.2f%n", result1, result2, result3, result4);
+    }
+}
